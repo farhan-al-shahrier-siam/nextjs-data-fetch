d7bd3ad4bd6fa1785f401145bf54f7494c559cde
farhan-al-shahrier-siam <farhanalshahriersiam@gmail.com>
2026-04-18 21:40:34 +0600

    Exploring different type pf data fetching

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,8 +1,36 @@
+/* const getPosts = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    return res.json();
+}; */
 
-const PostsPage = () => {
+const getPosts = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+    if(!res.ok){
+        throw new Error("Failed to fetch posts...")
+    }
+
+    return res.json();
+};
+
+/* const getPosts = async () => {
+    try{
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    return res.json();
+    } catch(err){
+        throw new Error("Failed to fetch posts...")
+    }
+}; */
+
+const PostsPage = async () => {
+    /* const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const posts = await res.json();*/
+    
+    const posts = await getPosts()
+    console.log(posts) 
     return (
         <div>
-            <h2>Posts are comming soon</h2>
+            <h2>Posts are comming soon: {posts.length}</h2>
         </div>
     );
 };
